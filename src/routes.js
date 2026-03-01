85bf9c4b05e76e85130c5db4f63bb7164e2c54e2
Hasbi <habdwaheed@gmail.com>
2026-03-02 01:29:04 +0700

    feat: Create routes for note

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import {createNote, deleteNoteById, getNoteById, getNotes, updateNoteById} from './controller.js'
+
+// Router initialization
+const router = express.Router()
+
+// Add a new note route
+router.post('/notes', createNote)
+
+// Get all notes route
+router.get('/notes', getNotes)
+
+// Get note details by id route
+router.get('/notes/:id', getNoteById)
+
+// Update note data by id route
+router.put('/notes/:id', updateNoteById)
+
+// Delete a note by id route
+router.delete('/notes/:id', deleteNoteById)
+
+export default router
